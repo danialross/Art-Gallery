8ff920ba39fae29df999498c25bf7dc76ee291fe
danialross <danialrossar@gmail.com>
2025-01-11 12:09:43 +0800

    add favicons

--- art-gallery/src/app/layout.tsx
+++ art-gallery/src/app/layout.tsx
@@ -4,7 +4,15 @@ import Topbar from "@/components/Topbar";
 import { ReactNode } from "react";
 
 export const metadata: Metadata = {
-  title: "Art Gallery",
+    title: "Art Gallery",
+    icons: [
+        { rel: 'icon', url: '/favicon.ico' }, // Default favicon
+        { rel: 'icon', url: '/favicon-16x16.png', sizes: '16x16' }, // 16x16 favicon
+        { rel: 'icon', url: '/favicon-32x32.png', sizes: '32x32' }, // 32x32 favicon
+        { rel: 'apple-touch-icon', url: '/apple-touch-icon.png' }, // Apple touch icon
+        { rel: 'icon', url: '/android-chrome-192x192.png', sizes: '192x192' }, // Android Chrome 192x192
+        { rel: 'icon', url: '/android-chrome-512x512.png', sizes: '512x512' },
+    ]
 };
 
 export default function RootLayout({
